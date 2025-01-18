dedf91a5d97aab9ee7c95692af088981dd058cd4
RiaruAzaki <riaruazaki@proton.me>
2025-01-17 21:33:37 -0500

    Updated added various vscodium extensions

--- users/riaru/programs/vscodium/default.nix
+++ users/riaru/programs/vscodium/default.nix
@@ -29,6 +29,8 @@
         ecmel.vscode-html-css
         ms-vscode.live-server
         naumovs.color-highlight
+        stylelint.vscode-stylelint # TODO: fix dependency
+        meganrogge.template-string-converter
 
         # CSV
         grapecity.gc-excelviewer
@@ -56,6 +58,7 @@
 
         # C++
         llvm-vs-code-extensions.vscode-clangd
+        ms-vscode.hexeditor
 
         # Python
         ms-python.python
@@ -71,15 +74,27 @@
         pkief.material-product-icons
         supermaven.supermaven
         eamodio.gitlens
+        gruntfuggly.todo-tree
+
+        # Look at later
+        # https://marketplace.visualstudio.com/items?itemName=usernamehw.commands
+        # https://marketplace.visualstudio.com/items?itemName=sleistner.vscode-fileutils
+        # https://marketplace.visualstudio.com/items?itemName=kameshkotwani.google-search
+        # https://marketplace.visualstudio.com/items?itemName=pranaygp.vscode-css-peek
+        # https://marketplace.visualstudio.com/items?itemName=maptz.regionfolder
+        # https://marketplace.visualstudio.com/items?itemName=alefragnani.Bookmarks
+        # https://marketplace.visualstudio.com/items?itemName=urbantrout.refactor-css
+        # https://marketplace.visualstudio.com/items?itemName=tahabasri.snippets
       ]
       ++ (with extensions.open-vsx; [
         tauri-apps.tauri-vscode
         littensy.charmed-icons
 
-        # dtoplak.vscode-glsllint
+        dtoplak.vscode-glsllint # TODO: fix dependency
         slevesque.shader
-
+        hideoo.toggler
         yandeu.five-server
+        # gencer.html-slim-scss-css-class-completion # TODO confirm working
 
         # Python
         kevinrose.vsc-python-indent
@@ -88,9 +103,16 @@
       ++ (with extensions.vscode-marketplace; [
         bbenoist.qml
 
+        philsinatra.nested-comments
+
         # Typescript
         oven.bun-vscode
         wallabyjs.quokka-vscode
+        wallabyjs.console-ninja # TODO: fix dependency
+
+        csstools.postcss
+        joy-yu.css-snippets
+        formulahendry.auto-rename-tag
       ]);
 
     userSettings = {
