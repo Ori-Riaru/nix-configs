f13c6112efaef13dd5204f92b9c7c764310ffbf2
Ori Riaru <ori-riaru@proton.me>
2025-11-11 17:13:25 -0500

    fix(kumo): disable unused services

--- systems/kumo/configuration.nix
+++ systems/kumo/configuration.nix
@@ -8,14 +8,14 @@
     ../../configs/services/samba.nix
     ../../configs/services/mastodon/mastodon.nix
     ../../configs/services/nextcloud.nix
-    ../../configs/services/factorio.nix
+    # ../../configs/services/factorio.nix
     ../../configs/services/tailscale.nix
     ../../configs/services/nfs.nix
     ../../configs/services/restic.nix
     ../../configs/services/jellyfin.nix
     ../../configs/services/blocky.nix
     ../../configs/services/glance.nix
-    ../../configs/services/minecraft.nix
+    # ../../configs/services/minecraft.nix
     ../../configs/services/inadyn.nix
 
     ../../configs/programs/sops.nix
@@ -29,7 +29,6 @@
   environment.variables = {
     NH_FLAKE = lib.mkForce "/data/nfs/riaru/Projects/nix-configs";
   };
-  
 
   # Bootloader
   boot.loader.grub.enable = true;
