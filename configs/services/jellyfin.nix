1951839a4ff9f880b23bd09e8447f096a01ba773
Ori Riaru <ori-riaru@proton.me>
2025-11-11 17:12:57 -0500

    fix(jellyfin): setup ssl cert

--- configs/services/jellyfin.nix
+++ configs/services/jellyfin.nix
@@ -1,12 +1,21 @@
 {pkgs, ...}: {
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-  };
+  services.jellyfin.enable = true;
 
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
     pkgs.jellyfin-ffmpeg
   ];
+
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."riaru.undo.it" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://localhost:8096";
+      proxyWebsockets = true;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [80 443];
 }
