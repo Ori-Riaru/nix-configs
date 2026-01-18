cb74f0b951981d0ee5cb9a49442da9800e4ee762
Ori Riaru <ori-riaru@proton.me>
2026-01-18 04:08:30 -0500

    style(discord): remove old comments

--- configs/programs/discord.nix
+++ configs/programs/discord.nix
@@ -1,12 +1,4 @@
 {
-  # home.packages = with pkgs; [
-  #   (discord.override {
-  #     withVencord = true;
-  #     withOpenASAR = true;
-  #   })
-  # ];
-
-  # Switched to Vesktop because of https://github.com/NixOS/nixpkgs/issues/195512
   programs.vesktop = {
     enable = true;
 
