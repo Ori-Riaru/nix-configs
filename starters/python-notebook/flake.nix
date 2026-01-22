dc15bbb873abe2ccff39a484adb290c70372fa0c
Ori Riaru <ori-riaru@proton.me>
2026-01-22 10:50:40 -0500

    feat(python-notebook): create jupyter kernel entry to help with identifying correct python kernel version

--- starters/python-notebook/flake.nix
+++ starters/python-notebook/flake.nix
@@ -14,24 +14,48 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
-        config = {allowUnfree = true;};
+        config.allowUnfree = true;
       };
+
+      pythonWithPkgs =
+        pkgs.python3.withPackages
+        (python-pkgs:
+          with python-pkgs; [
+            ruff
+            ipython
+            ipykernel
+            jupyter
+            notebook
+            # numpy
+            # pandas
+            # matplotlib
+            # seaborn
+          ]);
     in {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          (python3.withPackages (python-pkgs:
-            with python-pkgs; [
-              ruff
-              ipython
-              ipykernel
-              jupyter
-              notebook
-              pip
-            ]))
+        packages = [
+          pythonWithPkgs
         ];
 
         shellHook = ''
+          # Register python kernel
+          mkdir -p .jupyter/kernels/nix_python
+          cat > .jupyter/kernels/nix_python/kernel.json <<EOF
+          {
+            "argv": [
+              "${pythonWithPkgs}/bin/python",
+              "-m",
+              "ipykernel_launcher",
+              "-f",
+              "{connection_file}"
+            ],
+            "display_name": "Python (Nix)",
+            "language": "python",
+            "metadata": { "debugger": true }
+          }
+          EOF
 
+          export JUPYTER_PATH="$PWD/.jupyter:$JUPYTER_PATH"
         '';
       };
     });
