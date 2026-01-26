5fdfef547a12d890e430f62622a68c3252fa0aee
Ori Riaru <ori-riaru@proton.me>
2026-01-26 11:26:48 -0500

    fix(n8n): allow access form local host

--- configs/services/n8n.nix
+++ configs/services/n8n.nix
@@ -1,3 +1,10 @@
-{
-  services.n8n.enable = true;
+{...}: {
+  services.n8n = {
+    enable = true;
+    openFirewall = true;
+    environment = {
+      "N8N_SECURE_COOKIE" = "false";
+      "NODES_EXCLUDE" = "[ ]";
+    };
+  };
 }
