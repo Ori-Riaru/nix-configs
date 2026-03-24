52a09058ce616bc567988a162c26305c7f382060
Ori Riaru <ori-riaru@proton.me>
2026-03-24 03:06:19 -0400

    fix(starship): fix incorrect foreground color

--- configs/programs/starship.nix
+++ configs/programs/starship.nix
@@ -14,6 +14,7 @@
       palette = "neutral_dark";
       palettes.neutral_dark = {
         text = "${settings.text}";
+        foreground = "${settings.base}";
         subtext = "${settings.subtext}";
         background = "${settings.card}";
         red = "${settings.red}";
@@ -34,12 +35,12 @@
         disabled = false;
         format = "$symbol";
 
-        success_symbol = "[](fg:green)[](bold bg:green)[](fg:green) ";
-        error_symbol = "[](fg:red)[](bold bg:red)[](fg:red) ";
+        success_symbol = "[](fg:green)[](bold fg:foreground bg:green)[](fg:green) ";
+        error_symbol = "[](fg:red)[](bold fg:foreground bg:red)[]( fg:red) ";
       };
 
       cmd_duration = {
-        format = "[](fg:orange)[$duration](bg:orange)[](fg:orange) ";
+        format = "[](fg:orange)[$duration](fg:foreground bg:orange)[](fg:orange) ";
       };
 
       directory = {
@@ -71,90 +72,90 @@
       git_branch = {
         symbol = "";
         style = "bg:cyan";
-        format = "[](fg:cyan)[[ $symbol $branch ](fg:text bg:cyan)]($style)";
+        format = "[](fg:cyan)[[ $symbol $branch ](fg:foreground bg:cyan)]($style)";
       };
 
       git_status = {
         style = "bg:cyan";
-        format = "[[($all_status$ahead_behind )](fg:text bg:cyan)]($style)[](fg:cyan) ";
+        format = "[[($all_status$ahead_behind )](fg:foreground bg:cyan)]($style)[](fg:cyan) ";
       };
 
       nodejs = {
         symbol = "";
         style = "bg:blue";
-        format = "[](fg:green)[[$symbol( $version)](fg:text bg:green)]($style)[](fg:green) ";
+        format = "[](fg:green)[[$symbol( $version)](fg:foreground bg:green)]($style)[](fg:green) ";
       };
 
       c = {
         symbol = " ";
         style = "bg:blue";
-        format = "[](fg:blue)[[$symbol( $version)](fg:text bg:blue)]($style)[](fg:blue) ";
+        format = "[](fg:blue)[[$symbol( $version)](fg:foreground bg:blue)]($style)[](fg:blue) ";
       };
 
       rust = {
         symbol = "";
         style = "bg:orange";
-        format = "[](fg:orange)[[$symbol( $version)](fg:text bg:orange)]($style)[](fg:orange) ";
+        format = "[](fg:orange)[[$symbol( $version)](fg:foreground bg:orange)]($style)[](fg:orange) ";
       };
 
       nix_shell = {
         symbol = "";
         style = "bg:blue";
-        format = "[](fg:blue)[[$symbol( $version)](fg:text bg:blue)]($style)[](fg:blue) ";
+        format = "[](fg:blue)[[$symbol( $version)](fg:foreground bg:blue)]($style)[](fg:blue) ";
       };
 
       golang = {
         symbol = "";
         style = "bg:blue";
-        format = "[](fg:blue)[[$symbol( $version)](fg:text bg:blue)]($style)[](fg:blue) ";
+        format = "[](fg:blue)[[$symbol( $version)](fg:foreground bg:blue)]($style)[](fg:blue) ";
       };
 
       php = {
         symbol = "";
         style = "bg:blue";
-        format = "[](fg:purple)[[$symbol( $version)](fg:text bg:blue)]($style)[](fg:purple) ";
+        format = "[](fg:purple)[[$symbol( $version)](fg:foreground bg:blue)]($style)[](fg:purple) ";
       };
 
       java = {
         symbol = " ";
         style = "bg:blue";
-        format = "[](fg:red)[[$symbol( $version)](fg:text bg:red)]($style)[](fg:red) ";
+        format = "[](fg:red)[[$symbol( $version)](fg:foreground bg:red)]($style)[](fg:red) ";
       };
 
       kotlin = {
         symbol = "";
         style = "bg:blue";
-        format = "[](fg:purple)[[ $symbol( $version) ](fg:text bg:purple)]($style)[](fg:purple) ";
+        format = "[](fg:purple)[[ $symbol( $version) ](fg:foreground bg:purple)]($style)[](fg:purple) ";
       };
 
       haskell = {
         symbol = "";
         style = "bg:purple";
-        format = "[](fg:purple)[[ $symbol( $version) ](fg:text bg:purple)]($style)[](fg:purple) ";
+        format = "[](fg:purple)[[ $symbol( $version) ](fg:foreground bg:purple)]($style)[](fg:purple) ";
       };
 
       python = {
         symbol = "";
         style = "bg:yellow";
-        format = "[](fg:yellow)[[ $symbol($version) ](fg:text bg:yellow)]($style)[](fg:yellow) ";
+        format = "[](fg:yellow)[[ $symbol($version) ](fg:foreground bg:yellow)]($style)[](fg:yellow) ";
       };
 
       docker_context = {
         symbol = "";
         style = "bg:bg3";
-        format = "[](fg:blue)[[ $symbol( $context) ](fg:text bg:blue)]($style)[](fg:blue) ";
+        format = "[](fg:blue)[[ $symbol( $context) ](fg:foreground bg:blue)]($style)[](fg:blue) ";
       };
 
       username = {
         show_always = true;
-        style_user = "bg:purple fg:text";
-        style_root = "bg:purple fg:text";
+        style_user = "bg:purple fg:foreground";
+        style_root = "bg:purple fg:foreground";
         format = "[](fg:purple)[ $user ]($style)[](fg:purple)";
       };
 
       hostname = {
         ssh_only = true;
-        style = "bg:purple fg:text";
+        style = "bg:purple fg:foreground";
         format = " [](fg:purple)[ @$hostname ]($style)[](fg:purple) ";
       };
 
