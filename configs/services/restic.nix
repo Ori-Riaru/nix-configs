e6f2b56728f4650bc2b7a1b4f2a0a821c6e9b864
Ori Riaru <ori-riaru@proton.me>
2026-03-24 03:05:27 -0400

    feat(restic): add backup for various services

--- configs/services/restic.nix
+++ configs/services/restic.nix
@@ -1,7 +1,13 @@
 # mount backups by: sudo restic-home mount --allow-other /mnt/restic
 {config, ...}: {
   systemd.tmpfiles.rules = [
-    "d /mnt/bulk/Backups/restic/home 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/home 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/nextcloud 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/mastodon 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/matrix 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/jellyfin 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/minecraft 0755 root root -"
+    "d /mnt/bulk/data/Backups/restic/palworld 0755 root root -"
   ];
 
   environment.variables = {
@@ -15,6 +21,108 @@
       initialize = true;
       paths = ["/data/nfs/riaru"];
       exclude = [];
+      passwordFile = config.sops.secrets.restic_pass.path;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    };
+
+    # nextcloud = {
+    #   repository = "/mnt/bulk/data/Backups/restic/nextcloud";
+    #   initialize = true;
+    #   paths = ["/data/nextcloud"];
+    #   passwordFile = config.sops.secrets.restic_pass.path;
+    #   pruneOpts = [
+    #     "--keep-daily 7"
+    #     "--keep-weekly 4"
+    #     "--keep-monthly 12"
+    #     "--keep-yearly 10"
+    #   ];
+    #   timerConfig = {
+    #     OnCalendar = "daily";
+    #     Persistent = true;
+    #   };
+    # };
+
+    # mastodon = {
+    #   repository = "/mnt/bulk/data/Backups/restic/mastodon";
+    #   initialize = true;
+    #   paths = ["/var/lib/mastodon"];
+    #   passwordFile = config.sops.secrets.restic_pass.path;
+    #   pruneOpts = [
+    #     "--keep-daily 7"
+    #     "--keep-weekly 4"
+    #     "--keep-monthly 12"
+    #     "--keep-yearly 10"
+    #   ];
+    #   timerConfig = {
+    #     OnCalendar = "daily";
+    #     Persistent = true;
+    #   };
+    # };
+
+    matrix = {
+      repository = "/mnt/bulk/data/Backups/restic/matrix";
+      initialize = true;
+      paths = ["/var/lib/continuwuity"];
+      passwordFile = config.sops.secrets.restic_pass.path;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    };
+
+    jellyfin = {
+      repository = "/mnt/bulk/data/Backups/restic/jellyfin";
+      initialize = true;
+      paths = ["/var/lib/jellyfin"];
+      passwordFile = config.sops.secrets.restic_pass.path;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    };
+
+    minecraft = {
+      repository = "/mnt/bulk/data/Backups/restic/minecraft";
+      initialize = true;
+      paths = ["/data/minecraft"];
+      passwordFile = config.sops.secrets.restic_pass.path;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    };
+
+    palworld = {
+      repository = "/mnt/bulk/data/Backups/restic/palworld";
+      initialize = true;
+      paths = ["/data/palworld"];
       passwordFile = config.sops.secrets.restic_pass.path;
       pruneOpts = [
         "--keep-daily 7"
