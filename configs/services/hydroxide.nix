0ef8770b8473bb6bbac1a5d84810804c768dd342
Ori Riaru <ori-riaru@proton.me>
2026-04-10 23:32:19 -0400

    feat(hydroxide): setup hydroxide proton mail bridge

--- configs/services/hydroxide.nix
+++ configs/services/hydroxide.nix
@@ -5,14 +5,27 @@
 
   networking.firewall.allowedTCPPorts = [1143];
 
+  users = {
+    groups.hydroxide = {};
+    users.hydroxide = {
+      isSystemUser = true;
+      group = "hydroxide";
+      home = "/var/lib/hydroxide";
+      createHome = true;
+    };
+  };
+
   systemd.services.hydroxide = {
-    description = "Hydroxide ProtonMail bridge";
+    description = "Hydroxide";
     wantedBy = ["multi-user.target"];
     after = ["network.target"];
 
     serviceConfig = {
-      ExecStart = "${pkgs.hydroxide}/bin/hydroxide -imap-host 0.0.0.0 -imap-port 1143 imap";
+      ExecStart = "${pkgs.hydroxide}/bin/hydroxide -smtp-host 0.0.0.0 -smtp-port 1025 -imap-host 0.0.0.0 -imap-port 1143 -disable-carddav serve";
       Restart = "on-failure";
+      RestartSec = 5;
+      User = "hydroxide";
+      Group = "hydroxide";
     };
   };
 }
