29ae8fd548e4f8de0239bcb3e8b4b772fab446ae
Ori Riaru <ori-riaru@proton.me>
2026-05-11 03:44:07 -0400

    feat(waybar): disable waybar by default

--- configs/groups/niri-desktop.nix
+++ configs/groups/niri-desktop.nix
@@ -3,7 +3,7 @@
     ../system/wayland-hints.nix
     ../programs/niri/niri.nix
     ../programs/walker/walker.nix
-    ../programs/waybar/waybar.nix
+    # ../programs/waybar/waybar.nix
     ../programs/mako.nix
     ../programs/libnotify.nix
     ../programs/hypridle.nix
