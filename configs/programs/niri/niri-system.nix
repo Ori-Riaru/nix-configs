edc2d738e9f436b69718c659639fc81f68e65d66
Ori Riaru <ori-riaru@proton.me>
2026-05-26 02:35:09 -0400

    feat(niri): switch to gnome polkit instead kde polkit

--- configs/programs/niri/niri-system.nix
+++ configs/programs/niri/niri-system.nix
@@ -1,14 +1,30 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   imports = [
     inputs.niri.nixosModules.niri
   ];
 
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  systemd.user.services.niri-flake-polkit = lib.mkForce {
+    description = "PolicyKit Authentication Agent provided by niri-flake";
+    wantedBy = ["niri.service"];
+    after = ["graphical-session.target"];
+    partOf = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  security.pam.services.swaylock = {};
 
   programs.niri.enable = true;
-
-  
 }
