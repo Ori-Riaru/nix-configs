f70ecb47b7e4518d8e123fe07514a3f74ca2e18b
Ori Riaru <ori-riaru@proton.me>
2026-05-26 02:37:58 -0400

    fix(ssh): disable removed nixpkgs options

--- configs/system/ssh-bypass.nix
+++ configs/system/ssh-bypass.nix
@@ -2,18 +2,18 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      "github.com" = {
-        hostname = "ssh.github.com";
-        port = 443;
-        user = "git";
-      };
-      "kumo" = {
-        proxyCommand = "none";
-      };
-      "*" = {
-        proxyCommand = "ssh -W %h:%p kumo";
-      };
-    };
+    # matchBlocks = {
+    #   "github.com" = {
+    #     hostname = "ssh.github.com";
+    #     port = 443;
+    #     user = "git";
+    #   };
+    #   "kumo" = {
+    #     proxyCommand = "none";
+    #   };
+    #   "*" = {
+    #     proxyCommand = "ssh -W %h:%p kumo";
+    #   };
+    # };
   };
 }
