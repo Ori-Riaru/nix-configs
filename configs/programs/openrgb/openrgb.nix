d378c590f5c33e0f1ebbb1468d80fa374499b998
Ori Riaru <ori-riaru@proton.me>
2026-06-02 11:52:47 -0400

    refactor(openrgb): use systemd service instead of niri startup apps

--- configs/programs/openrgb/openrgb.nix
+++ configs/programs/openrgb/openrgb.nix
@@ -2,4 +2,20 @@
   home.packages = with pkgs; [
     openrgb
   ];
+
+  systemd.user.services = {
+    openrgb-profile-off = {
+      Unit = {
+        Description = "OpenRGB Profile Off";
+        PartOf = ["niri.service"];
+        After = ["niri.service"];
+      };
+      Service = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.openrgb}/bin/openrgb --profile off";
+      };
+      Install.WantedBy = ["niri.service"];
+    };
+  };
 }
