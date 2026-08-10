f672b8a19318e901da28380abfeeebcdada693c3
Ori Riaru <ori-riaru@proton.me>
2026-08-10 03:32:54 -0400

    fix(wayland): add more platform hints to use wayland

--- configs/system/wayland-hints.nix
+++ configs/system/wayland-hints.nix
@@ -3,5 +3,12 @@
     NIXOS_OZONE_WL = "1";
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
     QT_QPA_PLATFORM = "wayland";
+    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    GDK_BACKEND = "wayland,x11";
+    ANKI_WAYLAND = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    XDG_SESSION_TYPE = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
   };
 }
