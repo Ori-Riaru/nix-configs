948b1075807bfd4ff92d6348924311ae4b4ea108
Ori Riaru <ori-riaru@proton.me>
2026-08-10 03:33:30 -0400

    fix(matrix): fix matrix call not working on local ip

--- configs/services/matrix.nix
+++ configs/services/matrix.nix
@@ -51,14 +51,12 @@ in {
       no-multicast-peers
       denied-peer-ip=0.0.0.0-0.255.255.255
       denied-peer-ip=10.0.0.0-10.255.255.255
-      denied-peer-ip=100.64.0.0-100.127.255.255
       denied-peer-ip=127.0.0.0-127.255.255.255
       denied-peer-ip=169.254.0.0-169.254.255.255
       denied-peer-ip=172.16.0.0-172.31.255.255
       denied-peer-ip=192.0.0.0-192.0.0.255
       denied-peer-ip=192.0.2.0-192.0.2.255
       denied-peer-ip=192.88.99.0-192.88.99.255
-      denied-peer-ip=192.168.0.0-192.168.255.255
       denied-peer-ip=198.18.0.0-198.19.255.255
       denied-peer-ip=198.51.100.0-198.51.100.255
       denied-peer-ip=203.0.113.0-203.0.113.255
@@ -69,8 +67,6 @@ in {
       denied-peer-ip=100::-100::ffff:ffff:ffff:ffff
       denied-peer-ip=2001::-2001:1ff:ffff:ffff:ffff:ffff:ffff:ffff
       denied-peer-ip=2002::-2002:ffff:ffff:ffff:ffff:ffff:ffff:ffff
-      denied-peer-ip=fc00::-fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
-      denied-peer-ip=fe80::-febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff
     '';
   };
 
@@ -122,6 +118,8 @@ in {
         port_range_start = 50100;
         port_range_end = 50200;
         use_external_ip = true;
+        advertise_internal_ip = true;
+        skip_external_ip_validation = true;
         enable_loopback_candidate = false;
         turn_servers = [
           {
