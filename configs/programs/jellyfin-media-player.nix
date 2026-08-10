53347a62071deaa90840d881d38dabaf7cbfa759
Ori Riaru <ori-riaru@proton.me>
2026-08-10 03:33:51 -0400

    fix(jellyfin-desktop): disable broken package

--- configs/programs/jellyfin-media-player.nix
+++ configs/programs/jellyfin-media-player.nix
@@ -1,9 +1,5 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    jellyfin-media-player
-  ];
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "qtwebengine-5.15.19"
+  home.packages = with pkgs.stable; [
+    # jellyfin-media-player
   ];
 }
